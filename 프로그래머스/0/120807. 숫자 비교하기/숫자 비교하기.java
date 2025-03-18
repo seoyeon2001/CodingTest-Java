e559b5315894d54adc20dd9b749563ec1ea91983
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-18 11:06:55 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        // int answer = 0;
-        if (num1 ==  num2) return 1;
-        else return -1;
-        // return answer;
+        return (num1 ==  num2) ? 1 : -1;
     }
 }
