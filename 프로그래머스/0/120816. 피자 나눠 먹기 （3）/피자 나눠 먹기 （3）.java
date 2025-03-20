2899eb51e53b3d4ae531c8813688aac20b5b8800
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-20 10:44:23 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int slice, int n) {
-        return (n + slice - 1) / slice;
+        // return (n + slice - 1) / slice;
+        return n % slice != 0 ? (n / slice) + 1 : n / slice;
     }
 }
