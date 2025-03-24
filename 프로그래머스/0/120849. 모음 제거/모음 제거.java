74c2ae7ecca76b9631a74a1cd57146a892274e29
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-24 11:25:48 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,14 +1,18 @@
 class Solution {
     public String solution(String my_string) {
         
+        // // 방법 1
         String answer = my_string.replace("a", "");
         answer = answer.replace("e", "");
         answer = answer.replace("i", "");
         answer = answer.replace("o", "");
         answer = answer.replace("u", "");
         
+        // // 방법 2
         // String answer = my_string.replaceAll("[aeiou]", "");
-        // String answer = myString.replaceAll("a|e|i|o|u", "");
+        
+        // 방법 3
+        // String answer = my_string.replaceAll("a|e|i|o|u", "");
         return answer;
     }
 }
