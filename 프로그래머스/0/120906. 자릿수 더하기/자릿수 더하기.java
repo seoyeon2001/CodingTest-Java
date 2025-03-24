d01516e6a657d36d10c04d372725ac1e71a16f62
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-24 17:08:18 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,13 +1,17 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String a = Integer.toString(n);
-        for (int i = 0; i < a.length(); i++) answer += Integer.parseInt(a.substring(i, i+1));
         
-        // while(n>0){
-        //     answer+=n%10;
-        //     n/=10;
-        // }
+        // // 방법 1
+        // String a = Integer.toString(n);
+        // for (int i = 0; i < a.length(); i++) answer += Integer.parseInt(a.substring(i, i+1));
+        
+        // 방법 2
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        
         return answer;
         
     }
