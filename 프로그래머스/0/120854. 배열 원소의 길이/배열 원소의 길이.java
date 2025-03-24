a7886e94a0c1b59f0495633e0a7c8d824db9d4d6
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-24 11:38:07 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,14 +1,10 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(String[] strlist) {
-        // int[] answer = new int[strlist.length];
-        // for ( int i = 0; i < strlist.length; i++) {
-        //     answer[i] = strlist[i].length();
-        // }
-        // return answer;
-        
-        return Arrays.stream(strlist).mapToInt(String::length).toArray();
+        int[] answer = new int[strlist.length];
+        for ( int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;        
     }
 }
 
