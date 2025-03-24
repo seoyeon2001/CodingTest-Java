5bd7196f0e1e30fe2d1e9f353a24cc552208ef14
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-24 11:17:41 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/README.md
+++ 프로그래머스/0/120849. 모음 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모음 제거 - 120849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 0.04 ms
+메모리: 86.7 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 11:16:15
+2025년 03월 24일 11:17:40
 
 ### 문제 설명
 
--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replace("a", "");
-        answer = answer.replace("e", "");
-        answer = answer.replace("i", "");
-        answer = answer.replace("o", "");
-        answer = answer.replace("u", "");
+        // String answer = my_string.replace("a", "");
+        // answer = answer.replace("e", "");
+        // answer = answer.replace("i", "");
+        // answer = answer.replace("o", "");
+        // answer = answer.replace("u", "");
+        
+        String answer = my_string.replaceAll("[aeiou]", "");
         return answer;
     }
 }
