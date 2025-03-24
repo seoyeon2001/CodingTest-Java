2672fad2bac1f40aa2e599ea9a9603757e9f73c8
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-24 17:47:33 +0900

    [level 0] Title: 문자열안에 문자열, Time: 1.98 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,15 +1,17 @@
 class Solution {
     public int solution(String str1, String str2) {
-        // int answer = 0;
-        // for (int i = 0; i < str1.length() - str2.length() + 1; i++) {
-        //     String tmp = str1.substring(i, i + str2.length());
-        //     System.out.println(tmp);
-        //     if (tmp.equals(str2)) {
-        //         return 1;
-        //     }
-        // }
-        // return 2;
+        // 방법 1
+        int answer = 0;
+        for (int i = 0; i < str1.length() - str2.length() + 1; i++) {
+            String tmp = str1.substring(i, i + str2.length());
+            System.out.println(tmp);
+            if (tmp.equals(str2)) {
+                return 1;
+            }
+        }
+        return 2;
         
-        return (str1.contains(str2) ? 1: 2);
+        // // 방법 2
+        // return (str1.contains(str2) ? 1: 2);
     }
 }
