f3fec23d07e02125bd5a614b7b4e2ea0e80af3ba
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 10:03:47 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,18 +1,17 @@
 class Solution {
     public int solution(int price) {
-        double result = 0;
+        int answer = 0;
         
         if (price >= 500000) {
-            result = price * 0.8;
+            answer = (int) (price * 0.8);
         } else if (price >= 300000) {
-            result = price * 0.9;
+            answer = (int) (price * 0.9);
         } else if (price >= 100000) {
-            result = price * 0.95;
+            answer = (int) (price * 0.95);
         } else {
-            result = (double) price;
+            answer = price;
         }
         
-        int answer = (int) result;
         return answer;
     }
 }
