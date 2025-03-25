ec5946fdaab74876d7c3545037de13dd9c506d0b
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 17:42:54 +0900

    [level 0] Title: 숫자 찾기, Time: 0.07 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int num, int k) {
-//         int answer = -1;
-//         String num_string = Integer.toString(num);
+        int answer = -1;
+        String num_string = Integer.toString(num);
         
-//         for (int i = 0; i < num_string.length(); i++) {            
-//             if (Character.getNumericValue(num_string.charAt(i)) == k) return i+1;
-//         }
+        for (int i = 0; i < num_string.length(); i++) {            
+            if (Character.getNumericValue(num_string.charAt(i)) == k) return i+1;
+        }
         
-//         return answer;
-        return (" " + num).indexOf(String.valueOf(k));
+        return answer;
+        // return (" " + num).indexOf(String.valueOf(k));
     }
 }
