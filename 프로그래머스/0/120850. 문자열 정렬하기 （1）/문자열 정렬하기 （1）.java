8625178f2ded9581e3ed5636ee963559cd97ba00
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 11:16:57 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.06 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -3,23 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
         
-//         ArrayList<Character> arrayList = new ArrayList<>();
+        // 방법 1
+        /*
+        ArrayList<Character> arrayList = new ArrayList<>();
         
-//         for(char ch: my_string.toCharArray()) {
-//             if (Character.isDigit(ch)) arrayList.add(ch); 
-//         }
+        for(char ch: my_string.toCharArray()) {
+            if (Character.isDigit(ch)) arrayList.add(ch); 
+        }
         
-//         Collections.sort(arrayList);
+        Collections.sort(arrayList);
         
-//         int[] answer = new int[arrayList.size()];
+        int[] answer = new int[arrayList.size()];
         
-//         for(int i = 0; i < arrayList.size(); i++) {
-//             answer[i] = Character.getNumericValue(arrayList.get(i));
-//         }
+        for(int i = 0; i < arrayList.size(); i++) {
+            answer[i] = Character.getNumericValue(arrayList.get(i));
+        }
     
-//         return answer;
-        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split(""))
-            .sorted().mapToInt(Integer::parseInt)
-            .toArray();
+        return answer;
+        */
+        
+        // 방법 2
+        my_string = my_string.replaceAll("[a-z]",""); // 대문자는 없으니까
+
+        int[] answer = new int[my_string.length()];
+
+        for(int i = 0; i < my_string.length(); i++){
+            answer[i] = my_string.charAt(i) - '0';
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+        
+        // // 방법 3 - stream
+        // return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+        
     }
 }
