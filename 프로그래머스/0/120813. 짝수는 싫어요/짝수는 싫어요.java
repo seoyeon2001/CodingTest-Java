057af603491ef9596b20cdefd48bc7a506de3afd
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 09:43:10 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,12 +3,19 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int n) {
         
-        ArrayList<Integer> answer_arraylist = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
         for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) answer_arraylist.add(i);
+            if (i % 2 == 1) arrayList.add(i);
         }
         
-        int[] answer = answer_arraylist.stream().mapToInt(Integer::intValue).toArray();
+        // int[] answer = arrayList.stream().mapToInt(Integer::intValue).toArray();
+        
+        int[] answer = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            answer[i] = arrayList.get(i);
+        }
+        
         return answer;
     }
 }
