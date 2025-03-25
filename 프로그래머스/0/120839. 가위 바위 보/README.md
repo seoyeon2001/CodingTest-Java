70a59fba9b693eeecf24f081541e5bd020552fdc
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 10:28:13 +0900

    [level 0] Title: 가위 바위 보, Time: 0.09 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/README.md
+++ 프로그래머스/0/120839. 가위 바위 보/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가위 바위 보 - 120839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 3.00 ms
+메모리: 89.1 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 10:24:33
+2025년 03월 25일 10:28:11
 
 ### 문제 설명
 
--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,3 +1,5 @@
+// 방법 1
+/*
 class Solution {
     public String solution(String rsp) {
         String answer = "";
@@ -9,5 +11,28 @@ class Solution {
         }
         
         return answer;
+    }
+}
+*/
+
+// 방법 2
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String rsp) {
+        //2 - 가위, 0 - 바위, 5 - 보
+        Map<String, String> winNumbers = new HashMap<>();
+        winNumbers.put("2", "0");
+        winNumbers.put("0", "5");
+        winNumbers.put("5", "2");
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < rsp.length(); i++) {
+            answer.append(winNumbers.get(rsp.substring(i, i+1)));
+        }
+
+        return answer.toString();
     }
 }
