fed11c9e6c73f7d1319b74663af230cc39530551
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 11:33:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.45 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (1) - 120851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851) 
 
 ### 성능 요약
 
-메모리: 89.5 MB, 시간: 0.08 ms
+메모리: 72.3 MB, 시간: 0.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 11:32:10
+2025년 03월 25일 11:33:48
 
 ### 문제 설명
 
--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,9 +1,19 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        for(char ch: my_string.toCharArray()) {
-            if(Character.isDigit(ch)) answer += ch - '0';
+        
+        // // 방법 1
+        // for(char ch: my_string.toCharArray()) {
+        //     if(Character.isDigit(ch)) answer += ch - '0';
+        // }
+        
+        // 방법 2
+        String str = my_string.replaceAll("[^0-9]","");
+
+        for(char ch : str.toCharArray()) {
+            answer += Character.getNumericValue(ch);
         }
+
         return answer;
     }
 }
