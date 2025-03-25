3bc47883947a388651c66a6b8fb9f7157906949f
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-03-25 11:11:11 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.20 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        
+        ArrayList<Character> arrayList = new ArrayList<>();
+        
+        for(char ch: my_string.toCharArray()) {
+            if (Character.isDigit(ch)) arrayList.add(ch); 
+        }
+        
+        Collections.sort(arrayList);
+        
+        int[] answer = new int[arrayList.size()];
+        
+        for(int i = 0; i < arrayList.size(); i++) {
+            answer[i] = Character.getNumericValue(arrayList.get(i));
+        }
+    
+        return answer;
+    }
+}
