4d67370d517cac522355a9541ba3d247aa41cdbe
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-04-02 09:45:57 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main{
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
@@ -12,10 +12,12 @@ public class Main{
         
         int sum = a + b;
         
-        bw.write(sum+"\n");
+        // bw.write(sum+"\n");
         
         br.close(); 
-        bw.flush();
-        bw.close();
+        // bw.flush();
+        // bw.close();
+        
+        System.out.println(sum);
     }
 }
