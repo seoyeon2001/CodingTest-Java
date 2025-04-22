f81e22e8be29720ed1062c48546a0cc15094017b
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-04-22 10:49:18 +0900

    [Gold IV] Title: 테트로미노, Time: 604 ms, Memory: 36984 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int max = Integer.MIN_VALUE;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N;
+    static int M;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+//         System.out.println(Arrays.deepToString(arr));
+        br.close();
+
+        boolean[][] visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 1, arr[i][j], visited);
+                visited[i][j] = false;
+
+                // T 모양 체크
+                checkT(i, j);
+            }
+        }
+        System.out.println(max);
+    }
+
+    // 재귀 -> 종료 필수
+    static void dfs(int x, int y, int depth, int nowSum, boolean[][] visited) {
+        if (depth == 4) {
+            max = Math.max(max, nowSum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            // 범위를 벗어나면
+            if (ny < 0 || nx < 0 || nx >= N || ny >= M) continue;
+
+            if (!visited[nx][ny]) {
+                visited[nx][ny] = true;
+
+                dfs(nx, ny, depth + 1, nowSum + arr[nx][ny], visited);
+
+                visited[nx][ny] = false;
+            }
+        }
+
+    }
+
+    // 기준: 가운데
+    static void checkT(int x, int y) {
+        // ㅗ 모양: 위 + 좌우
+        if (x - 1 >= 0 && y - 1 >= 0 && y + 1 < M) {
+            int sum = arr[x][y] + arr[x - 1][y] + arr[x][y - 1] + arr[x][y + 1];
+            max = Math.max(max, sum);
+        }
+
+        // ㅜ 모양: 아래 + 좌우
+        if (x + 1 < N && y - 1 >= 0 && y + 1 < M) {
+            int sum = arr[x][y] + arr[x + 1][y] + arr[x][y - 1] + arr[x][y + 1];
+            max = Math.max(max, sum);
+        }
+
+        // ㅓ 모양: 좌 + 상하
+        if (x - 1 >= 0 && x + 1 < N && y - 1 >= 0) {
+            int sum = arr[x][y] + arr[x - 1][y] + arr[x + 1][y] + arr[x][y - 1];
+            max = Math.max(max, sum);
+        }
+
+        // ㅏ 모양: 우 + 상하
+        if (x - 1 >= 0 && x + 1 < N && y + 1 < M) {
+            int sum = arr[x][y] + arr[x - 1][y] + arr[x + 1][y] + arr[x][y + 1];
+            max = Math.max(max, sum);
+        }
+    }
+}
