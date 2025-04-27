a39e98c69a3270bbf87ee53eae4e02b2e1c11e24
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-04-27 19:46:46 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split("-");
+//        System.out.println(Arrays.toString(inputs));
+
+        StringTokenizer st;
+        int[] result = new int[inputs.length];
+        for(int i = 0; i < inputs.length; i++) {
+
+            if (inputs[i].contains("+")) {
+                st = new StringTokenizer(inputs[i], "+");
+
+                int tmp = 0;
+                while(st.hasMoreTokens()) {
+                    tmp += Integer.parseInt(st.nextToken());
+                }
+
+                result[i] = tmp;
+            } else {
+                result[i] = Integer.parseInt(inputs[i]);
+            }
+        }
+
+        int answer = result[0];
+        for (int i = 1; i < result.length; i++) {
+            answer -= result[i];
+        }
+
+        System.out.println(answer);
+    }
+}
