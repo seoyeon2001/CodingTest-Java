609dbfb5720ade79f3cdae7e87f6266f99672a31
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-05-19 23:33:08 +0900

    [Silver IV] Title: 스택 2, Time: 4416 ms, Memory: 269056 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> q = new ArrayDeque<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+
+            switch(Integer.parseInt(st.nextToken())) {
+                case 1:
+                    q.add(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    if(!q.isEmpty()) {
+                        System.out.println(q.pollLast());
+                        break;
+                    } else {
+                        System.out.println(-1);
+                        break;
+                    }
+                case 3:
+                    System.out.println(q.size());
+                    break;
+                case 4:
+                    System.out.println(q.isEmpty() ? 1 : 0);
+                    break;
+                case 5:
+                    if(!q.isEmpty()) {
+                        System.out.println(q.peekLast());
+                        break;
+                    } else {
+                        System.out.println(-1);
+                        break;
+                    }
+
+            }
+        }
+    }
+}
+
