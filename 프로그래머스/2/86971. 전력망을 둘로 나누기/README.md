60f44fe169b1f386f551415690dcc5cccedcee26
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-05-30 09:34:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.76 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 8.84 ms
+메모리: 74.2 MB, 시간: 6.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 18:57:46
+2025년 05월 30일 09:34:00
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,9 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-
         // 기본 세팅
-        List<Integer>[] graph = new ArrayList[n + 1];
+        List<Integer>[] graph = new ArrayList[n+1];
         for(int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
@@ -14,49 +13,49 @@ class Solution {
             graph[wire[1]].add(wire[0]);
         }
         // System.out.println(Arrays.toString(graph));
-
-        // 전선 끊기 -> n-1번 반복
-        for (int[] wire : wires) {
+        
+        // 시작
+        for(int[] wire : wires) {
             graph[wire[0]].remove(Integer.valueOf(wire[1]));
             graph[wire[1]].remove(Integer.valueOf(wire[0]));
+            
             // System.out.println(Arrays.toString(graph));
-
-            int cnt1 = check(graph, n);
+            
+            int cnt1 = count(graph, n);
             int cnt2 = n - cnt1;
-
-            answer = Math.min(answer, Math.abs(cnt1-cnt2));
-
-            // 다시 붙이기
+            int cntR = cnt1 - cnt2;
+            answer = Math.min(answer, Math.abs(cntR));
+            
             graph[wire[0]].add(wire[1]);
             graph[wire[1]].add(wire[0]);
         }
-
+        
         return answer;
     }
-
-    static int check(List<Integer>[] graph, int n) {
+    
+    static int count(List<Integer>[] graph, int n) {
         int cnt = 0;
-
+        
         Deque<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n + 1];
-
+        boolean[] visited = new boolean[n+1];
+        
         q.add(1);
         visited[1] = true;
-
+        
         while(!q.isEmpty()) {
-            int v = q.poll();
-
-            for (int node : graph[v]) {
-                if (!visited[node]) {
-                    visited[node] = true;
+            int num = q.poll();
+            
+            for(int node : graph[num]) {
+                if(!visited[node]) {
                     q.add(node);
+                    visited[node] = true;
                 }
             }
         }
-        for (boolean b : visited) {
+        
+        for(boolean b : visited) {
             if (b) cnt++;
         }
-        // System.out.println(cnt);
         return cnt;
     }
 }
