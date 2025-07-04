76a687efdaf251793533d94c62e854550b7e27f5
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-07-04 17:44:40 +0900

    [level 4] Title: 도둑질, Time: 28.03 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -8,30 +8,22 @@ class Solution {
         int[] dp1 = new int[n];
         int[] dp2 = new int[n];
         
-        // 첫번째 포함 안해 = 마지막 포함
-        int[] newMoney = new int[n];
-        newMoney[0] = money[n-1];
-        for (int i = 1; i < n; i++) {
-            newMoney[i] = money[n-1-i];
-        }
-        dp1[0] = newMoney[0];
-        dp1[1] = newMoney[0];
-        for(int i = 2; i < n-1; i++) {
-            dp1[i] = Math.max(dp1[i-1], (dp1[i-2] + newMoney[i]));
+        // 첫번째 포함 안해
+        dp1[1] = money[1];
+        dp1[2] = Math.max(money[1], money[2]);
+        for(int i = 3; i < n; i++) {
+            dp1[i] = Math.max(dp1[i-1], (dp1[i-2] + money[i]));
         }
         
         // 첫번째 포함해 = 마지막 포함 안해
         dp2[0] = money[0];
-        dp2[1] = money[0];
+        dp2[1] = dp2[0];
         for(int i = 2; i < n-1; i++) {
             dp2[i] = Math.max(dp2[i-1], (dp2[i-2] + money[i]));
         }
         
         // System.out.println(Arrays.toString(dp1));
         // System.out.println(Arrays.toString(dp2));
-        
-        // Arrays.sort(dp1);
-        // Arrays.sort(dp2);
-        return Math.max(dp1[n-2], dp2[n-2]);
+        return Math.max(dp1[n-1], dp2[n-2]);
     }
 }
