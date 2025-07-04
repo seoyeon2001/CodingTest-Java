b533b2a63e1ee2464976d3f9e5b4871614ae1bc0
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-07-04 13:42:38 +0900

    [level 3] Title: 광고 삽입, Time: 277.80 ms, Memory: 294 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,37 +1,30 @@
 import java.util.*;
+
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        int playSec = stringToSec(play_time);
-        long[] dp = new long[playSec + 1];
+        long[] dp = new long[stringToSec(play_time) + 1];
+        int time = stringToSec(adv_time);
         
-        // 광고 길이
-        int advSec = stringToSec(adv_time);
-        
-        // dp +1, -1 채우기
         for (String log : logs) {
-            String[] startEnd = log.split("-");
-            int s = stringToSec(startEnd[0]);
-            int e = stringToSec(startEnd[1]);
+            String[] a = log.split("-");
+            int s = stringToSec(a[0]);
+            int e = stringToSec(a[1]);
+            
             dp[s]++;
             dp[e]--;
         }
         
-        // 해당 시점에 시청자 수 구하기
-        for (int i = 1; i < dp.length; i++) {
+        for(int i = 1; i < dp.length; i++) {
             dp[i] += dp[i-1];
         }
-        
-        // 해당 시간까지의 누적합 구하기
-        for (int i = 1; i < dp.length; i++) {
+        for(int i = 1; i < dp.length; i++) {
             dp[i] += dp[i-1];
         }
-        
-        // 구간의 최대 관객 수
-        long max = dp[advSec-1];
+        long max = dp[time-1];
         int result = 0;
-        for (int i = 0; i < dp.length - advSec; i++) {
-            if (dp[i+advSec]-dp[i] > max) {
-                max = dp[i+advSec] - dp[i];
+        for (int i = 0; i < dp.length - time; i++) {
+            if (dp[i+time]-dp[i] > max) {
+                max = dp[i+time] - dp[i];
                 result = i+1;
             }
         }
@@ -39,30 +32,15 @@ class Solution {
         return secToString(result);
     }
     
-    static int stringToSec(String str) {
+    int stringToSec(String str) {
         String[] a = str.split(":");
-        int h = Integer.parseInt(a[0]);
-        int m = Integer.parseInt(a[1]);
-        int s = Integer.parseInt(a[2]);
-        return h*3600 + m*60 + s;
+        return Integer.parseInt(a[0]) * 3600 + Integer.parseInt(a[1]) * 60 + Integer.parseInt(a[2]);
     }
     
-    static String secToString(int sec) {
+    String secToString(int sec) {
         int h = sec / 3600;
-        String strH = isOne(h);
         int m = sec % 3600 / 60;
-        String strM = isOne(m);
         int s = sec % 3600 % 60;
-        String strS = isOne(s);
-        return strH + ":" + strM + ":" + strS;
-    }
-    
-    static String isOne(int num) {
-        String s = Integer.toString(num);
-        if (s.length() == 1) {
-            return "0" + s;
-        } else {
-            return s;
-        }
+        return String.format("%02d:%02d:%02d", h, m, s);
     }
 }
