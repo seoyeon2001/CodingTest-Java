64cb9ee6075bdb4206c4c31fbb0f1ebbf28f4e68
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-08-09 01:13:45 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.java
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.java
@@ -1,14 +1,18 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[117];
-        dp[1] = dp[2] = dp[3] = 1;
+        
+        long[] arr = new long[117]; // 1 + 1 <= n + 1 <= 116 + 1
+        
+        arr[1] = arr[2] = arr[3] = 1;
         for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-3];
+            arr[i] = arr[i-1] + arr[i-3];
         }
-        System.out.print(dp[n]);
+        System.out.println(arr[n]);
     }
 }
