ea8b229a2491f48d5d727eda385ff704e9926bc4
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-08-11 20:50:53 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.16 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -3,29 +3,42 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        StringBuffer sb = new StringBuffer();
-        boolean isNextSpace = true;
+//         StringBuffer sb = new StringBuffer();
+//         boolean isNextSpace = true;
         
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == ' ') {
-                sb.append(" ");
-                isNextSpace = true;
-            }
+//         for(int i = 0; i < s.length(); i++) {
+//             if(s.charAt(i) == ' ') {
+//                 sb.append(" ");
+//                 isNextSpace = true;
+//             }
             
-            else if(isNextSpace) {
-                if(Character.isLetter(s.charAt(i))) {
-                    sb.append(Character.toUpperCase(s.charAt(i)));
-                } else {
-                    sb.append(s.charAt(i));
-                }
-                isNextSpace = false;
-            } 
+//             else if(isNextSpace) {
+//                 if(Character.isLetter(s.charAt(i))) {
+//                     sb.append(Character.toUpperCase(s.charAt(i)));
+//                 } else {
+//                     sb.append(s.charAt(i));
+//                 }
+//                 isNextSpace = false;
+//             } 
             
-            else {
-                sb.append(Character.toLowerCase(s.charAt(i)));
-            }
+//             else {
+//                 sb.append(Character.toLowerCase(s.charAt(i)));
+//             }
+//         }
+        
+//         return sb.toString();
+        
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        // System.out.println(Arrays.toString(sp));
+        
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
         }
-        
-        return sb.toString();
+
+        return answer;
     }
 }
