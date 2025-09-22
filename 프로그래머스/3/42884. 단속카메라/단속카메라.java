12cf85832c3c9cc632364cfbe0c41a0126722c19
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-09-22 14:16:14 +0900

    [level 3] Title: 단속카메라, Time: 11.08 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        
+        // 앞에 것부터 정렬
+        Arrays.sort(routes, (a, b) -> a[0] - b[0]);
+        
+        int min = routes[0][1];
+        
+        for(int i = 1; i < routes.length; i++) {
+            int s = routes[i][0];
+            int e = routes[i][1];
+            
+            if(s <= min) {
+                min = Math.min(min, e);
+            } else {
+                System.out.println(min);
+                min = e;
+                answer++;
+            }
+        }
+        
+        // System.out.println(Arrays.deepToString(routes));
+        return answer;
+    }
+}
