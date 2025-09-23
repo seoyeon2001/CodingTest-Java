ad8fc01f0bd0a4153dd42a4a5d2f063d20af1dbe
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-09-23 19:08:16 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 18944 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static boolean[] visited;
+    static int[][] graph;
+    static int[] plan;
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N];
+        graph = new int[N][N];
+        plan = new int[M];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(plan[0] - 1);
+
+        for (int p : plan) {
+            if (p > 0 && !visited[p - 1]) {
+                System.out.println("NO");
+                System.exit(0);
+            }
+        }
+
+        System.out.println("YES");
+    }
+
+    public static void dfs(int x) {
+        visited[x] = true;
+
+        for (int i = 0; i < N; i++) {
+            if (graph[x][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
