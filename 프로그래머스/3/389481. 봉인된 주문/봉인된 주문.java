0e225eef202cd3b245cab1d5528bb2294b2fe2a1
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-10-01 22:05:42 +0900

    [level 3] Title: 봉인된 주문, Time: 315.95 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -1,11 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public String solution(long n, String[] bans) {
-        String answer = "";
-        
+    public String solution(long n, String[] bans) {        
         long[] nums = new long[bans.length];
-        
         for(int j = 0; j < bans.length; j++) {
             long num = 0;
             int wordSize = bans[j].length();
@@ -16,7 +13,6 @@ class Solution {
             nums[j] = num;
         }
         Arrays.sort(nums);
-        // System.out.println(Arrays.toString(nums));
         
         int cnt = 0;
         for(long num : nums) {
@@ -25,45 +21,23 @@ class Solution {
             } else break;       
         }
         n += cnt;
-        // System.out.println(cnt);
         
-        return toWord(n);
-        
-//         List<Integer> word = new ArrayList<>();
-//         while(n > 0) {
-            
-//             int ahrt = (int) n / 26;
-//             int skajwl = (int) n % 26;
-            
-//             word.add(skajwl);
-            
-//             n = n / 26;            
-//         }
-//         // System.out.println(word);
-        
-//         StringBuilder sb = new StringBuilder();
-//         for(int i = word.size() - 1; i >= 0; i--) {
-//             sb.append((char) (word.get(i) + 96) + "");
-//         }
-        
-//         answer = sb.toString();
-        
-//         return answer;
-    }
-    
-    private String toWord(long n) {
-        long len = 1, count = 26, base = 0;
+        int len = 1;
+        long count = 26, base = 0;
         while (n > base + count) {
             base += count;
             count *= 26;
             len++;
         }
+        
         long offset = n - base - 1;
-        char[] word = new char[(int)len];
-        for (int i = (int)len - 1; i >= 0; i--) {
+        
+        char[] word = new char[len];
+        for (int i = len - 1; i >= 0; i--) {
             word[i] = (char)('a' + (offset % 26));
             offset /= 26;
         }
+        
         return new String(word);
     }
 }
