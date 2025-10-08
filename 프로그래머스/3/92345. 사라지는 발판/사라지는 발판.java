39201df1e9ae3cfe1bb9bead057b0d92c98ce825
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-10-08 21:20:41 +0900

    [level 3] Title: 사라지는 발판, Time: 16.92 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,3 +1,4 @@
+/*
 class Result {
     private boolean isWin; // 승리 여부
     private int moveCnt; // 움직임 횟수
@@ -88,5 +89,80 @@ class Solution {
     // 범위 내에 있는가?
     private boolean inRange(int r, int c) {
         return 0 <= r && r < row && 0 <= c && c < col;
+    }
+}
+*/
+
+/*
+* A = 나, B = 상대방
+* 만약 끝났을 때의 횟수가 
+* 짝수: 내가 지는 것, 홀수: 내가 이기는 것
+* 내가 지는 쪽이면 최대한 많이 움직여야 하고, 이기는 쪽이면 적게 움직여야 함
+* 이기는 경우가 1번이라도 있으면 무조건 이기는 경우를 선택해야 함 -> 그렇지 않으면 최선을 다하지 않은 것
+*/
+class Solution {
+    
+    class Point {
+        int x, y;
+        
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    // 상하좌우
+    int[] dr = {1, -1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
+    
+    int[][] board; // board 전역변수화
+    int row, col;
+    
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        this.board = board;
+        
+        row = board.length;
+        col = board[0].length;
+        
+        Point cntA = new Point(aloc[0], aloc[1]);
+        Point cntB = new Point(bloc[0], bloc[1]);
+
+        return dfs(cntA, cntB);
+    }
+    
+    
+	// me : 현재 턴인 사람 // you : 상대방
+    public int dfs(Point me, Point you) {
+        
+        int cr = me.x;
+        int cc = me.y;
+        
+        if(board[cr][cc] == 0) return 0;  // 발판이 사라졌다면 0 반환
+        
+        int result = 0; // 최종적인 턴 수
+        for(int i = 0; i < 4; i++){
+            int nr = cr + dr[i];
+            int nc = cc + dc[i];
+            
+            if(nr < 0 || nr >= row || nc < 0 || nc >= col || board[nr][nc] == 0) continue;
+            
+            board[cr][cc] = 0; // 이전에 서있던 곳으로 이동불가능하게 만듦
+            
+             // 여기서 상대방 턴이기 때문에 매개변수로 dfs(you,me) 순서로 들어간다.
+            int val = dfs(you, new Point(nr, nc)) + 1; // 턴수 + 1
+            
+            board[cr][cc] = 1; // 사용한 것을 원상 복구
+            
+            // 지금까지 모두 진 경우고, 이번(val)에 이겼을 때 -> 바로 이긴걸로 바꿔줌
+            if(result % 2 == 0 && val % 2 == 1) result = val;
+            
+            // 지금까지도 졌고, 이 경우(val)도 진 경우 -> 최대한 많이 움직인다.
+            else if(result % 2 == 0 && val % 2 == 0) result = Math.max(result, val);
+            
+            // 지금까지도 이겼고, 이 경우(val)도 이긴 경우 -> 최대한 적게 움직인다.
+            else if(result % 2 == 1 && val % 2 == 1) result = Math.min(result, val);
+        }
+        
+        return result;
     }
 }
