b5ab82b01b06492017bc5500879570a78d7907ad
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-10-29 03:30:27 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -8,38 +8,38 @@ public class Main {
 
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
-
-        int[] wArr = new int[w];
+        int[] height = new int[w];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < w; i++) {
-            wArr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < w; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] leftMax = new int[w];
+        int[] rightMax = new int[w];
+
+        // 왼쪽에서의 최대 높이 계산
+        leftMax[0] = height[0];
+        for (int i = 1; i < w; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        // 오른쪽에서의 최대 높이 계산
+        rightMax[w - 1] = height[w - 1];
+        for (int i = w - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
 
         int result = 0;
 
-        for(int i = 1; i < w-1; i++) {
-            int leftMax = Integer.MIN_VALUE;
-            int rightMax = Integer.MIN_VALUE;
-
-            // 왼쪽
-            for(int j = 0; j < i; j++) {
-                leftMax = Math.max(leftMax, wArr[j]);
+        // 각 위치의 물 높이 계산
+        for (int i = 1; i < w - 1; i++) {
+            int minHeight = Math.min(leftMax[i], rightMax[i]);
+            if (minHeight > height[i]) {
+                result += minHeight - height[i];
             }
-
-            // 오른쪽
-            for(int j = i+1; j < w; j++) {
-                rightMax = Math.max(rightMax, wArr[j]);
-            }
-
-//            System.out.println("leftMax = " + leftMax + " rightMax = " + rightMax);
-
-            int height = Math.min(leftMax, rightMax);
-
-            int water = height - wArr[i];
-            if(water > 0) result += water;
         }
-        System.out.println(result);
 
+        System.out.println(result);
     }
 }
