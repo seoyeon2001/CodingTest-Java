55eed2d3a3185ee9a928d3b5452f094e9872bf18
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-10-29 03:30:55 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -8,38 +8,43 @@ public class Main {
 
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
+
         int[] height = new int[w];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < w; i++) {
+        for(int i = 0; i < w; i++) {
             height[i] = Integer.parseInt(st.nextToken());
         }
+
+        int result = method2(w, height);
+
+        System.out.println(result);
+    }
+    
+    static int method2(int w, int[] height) {
+        int result = 0;
 
         int[] leftMax = new int[w];
         int[] rightMax = new int[w];
 
-        // 왼쪽에서의 최대 높이 계산
+        // 왼쪽 최대 높이 계산
         leftMax[0] = height[0];
         for (int i = 1; i < w; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
         }
 
-        // 오른쪽에서의 최대 높이 계산
+        // 오른쪽 최대 높이 계산
         rightMax[w - 1] = height[w - 1];
         for (int i = w - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
         }
-
-        int result = 0;
 
         // 각 위치의 물 높이 계산
-        for (int i = 1; i < w - 1; i++) {
+        for (int i = 1; i < w-1; i++) {
             int minHeight = Math.min(leftMax[i], rightMax[i]);
-            if (minHeight > height[i]) {
-                result += minHeight - height[i];
-            }
+            if (minHeight > height[i]) result += minHeight - height[i];
         }
 
-        System.out.println(result);
+        return result;
     }
 }
