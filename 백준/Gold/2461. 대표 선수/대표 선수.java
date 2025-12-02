144c198b036e0bf6a22ff27ccd38e49a45045ad3
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-12-02 11:48:53 +0900

    [Gold II] Title: 대표 선수, Time: 1868 ms, Memory: 104444 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -2,45 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N*M][2];
+        int[][] arr = new int[N][M];
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
-                arr[M*i+j][0] = Integer.parseInt(st.nextToken());
-                arr[M*i+j][1] = i;
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(arr[i]);
         }
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
 
-        int left = 0, right = 0;
-        int[] cnt = new int[N];
-        int count = 1;
-        cnt[arr[0][1]] = 1;
+        int[] nums = new int[N];
+        for(int i = 0; i < N; i++) {
+            nums[i] = arr[i][0];
+        }
+        int[] numsIdx = new int[N];
 
-        int min = Integer.MAX_VALUE;
-        while(left <= right) {
-            if(count == N) {
-                min = Math.min(min, arr[right][0] - arr[left][0]);
-                if(cnt[arr[left][1]] == 1) count--;
-                cnt[arr[left][1]]--;
-                left++;
-            } else {
-                right++;
-                if(right == N*M) break;
-                if(cnt[arr[right][1]] == 0) count++;
-                cnt[arr[right][1]]++;
+        int answer = Integer.MAX_VALUE;
+
+        for (int time = 0; time < N*(M-1); time++) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int minIdx = -1;
+
+            for(int i = 0; i < N; i++) {
+                if(nums[i] <= min) {
+                    min = nums[i];
+                    minIdx = i;
+                }
+                max = Math.max(max, nums[i]);
             }
-
-
+            answer = Math.min(answer, max - min);
+            numsIdx[minIdx]++;
+            if(numsIdx[minIdx] == M) break;
+            nums[minIdx] = arr[minIdx][numsIdx[minIdx]];
         }
-        System.out.println(min);
+
+        System.out.println(answer);
     }
 }
