bbf63eac26cb6f756252b40f337f646dc90b68b3
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-02-12 20:11:35 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,14 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    public static int n, m, r, c;
-    public static int[][] map;
-    public static int dir;
+    static int n, m, r, c;
+    static int[][] map;
+    static int dir;
 
-    public static int[] dr = {-1, 0, 1, 0};
-    public static int[] dc = {0, 1, 0, -1};
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
-    public static int answer = 2;
+    static boolean[][] cleaned;
+
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +28,8 @@ public class Main {
         dir = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
+        cleaned = new boolean[n][m];
+
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++) {
@@ -33,29 +37,27 @@ public class Main {
             }
         }
 
-        map[r][c] = answer;
         sol(r, c, dir);
 
-        System.out.println(answer-1);
-    }
-
-    // 청소해야 하는가?
-    public static boolean isNeedClean(int r, int c) {
-        if(map[r][c] == 0) return true;
-        else return false;
+        System.out.println(answer);
     }
 
     public static void sol(int r, int c, int dir) {
-        // 이동 가능
-        for(int i = 0; i < 4; i++) { // 4번 체크
+
+        // 현재 위치 청소
+        if (!cleaned[r][c]) {
+            cleaned[r][c] = true;
+            answer++;
+        }
+
+        // 4방향 탐색 -> 이동 가능
+        for(int i = 0; i < 4; i++) {
             dir = (dir+3) % 4; // 반시계 방향
             int nr = r + dr[dir], nc = c + dc[dir]; // 다음 이동 위치
 
             if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue; // 범위 벗어나면 무시
 
-            if(isNeedClean(nr, nc)) {
-                answer++;
-                map[nr][nc] = answer;
+            if(map[nr][nc] == 0 && !cleaned[nr][nc]) {
                 sol(nr, nc, dir);
                 return;
             }
