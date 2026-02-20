8b3ba84adc4d564d017ffa11da92cc327bd57697
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-02-21 02:59:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 748 ms, Memory: 122624 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int[][] map;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("");
 
-        n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][n];
+        int[][] map = new int[n][n];
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < n; j++) {
@@ -21,15 +18,17 @@ public class Main {
             }
         }
 
+        // 2차원 누적합 배열 만들기
         int[][] sumMap = new int[n+1][n+1];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                sumMap[i+1][j+1] = map[i][j] + sumMap[i][j+1] + sumMap[i+1][j] - sumMap[i][j];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                sumMap[i][j] = sumMap[i-1][j] + sumMap[i][j-1] - sumMap[i-1][j-1] + map[i-1][j-1];
             }
         }
+
 //        System.out.println("sumMap = " + Arrays.deepToString(sumMap));
 
-        for(int i = 0; i < m; i++) {
+        while(m-- > 0) {
             st = new StringTokenizer(br.readLine());
 
             int x1 = Integer.parseInt(st.nextToken());
@@ -40,6 +39,7 @@ public class Main {
             int answer = sumMap[x2][y2] - sumMap[x1-1][y2] - sumMap[x2][y1-1] + sumMap[x1-1][y1-1];
             sb.append(answer).append("\n");
         }
+
         System.out.println(sb);
     }
 }
