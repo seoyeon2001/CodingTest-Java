53495b546418874395e11ee5ff708d641de49031
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-04 17:48:47 +0900

    [Gold III] Title: 게리맨더링, Time: 136 ms, Memory: 15308 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -34,6 +34,7 @@ public class Main {
                 graph[i].add(Integer.parseInt(st.nextToken()));
             }
         }
+        // System.out.println("graph = " + Arrays.toString(graph));
 
         answer = Integer.MAX_VALUE;
 
@@ -52,6 +53,7 @@ public class Main {
         System.out.println(answer);
     }
 
+    // 조합 구하기
     static void combi(List<Integer> aList, int start, int r) {
         if(r == 0) {
             cal(aList);
@@ -60,16 +62,14 @@ public class Main {
 
         for(int i = start; i <= n; i++) {
             aList.add(i);
-            combi(aList, i + 1, r - 1);
-            aList.remove(aList.size() - 1);
+            combi(aList, i+1, r-1);
+            aList.remove(aList.size()-1);
         }
     }
 
     static void cal(List<Integer> aList) {
         // A구가 잘 연결되어있는지 확인
-        if(!isLinked(aList.get(0), aList, aList.size())) {
-            return;
-        }
+        if(!isLinked(aList)) return;
 
         List<Integer> bList = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
@@ -80,9 +80,7 @@ public class Main {
         }
 
         // B구가 잘 연결되어있는지 확인
-        if(!isLinked(bList.get(0), bList, bList.size())) {
-            return;
-        }
+        if(!isLinked(bList)) return;
 
         int resultA = 0, resultB = 0;
 
@@ -98,33 +96,30 @@ public class Main {
 
         int result = Math.abs(resultA - resultB);
         answer = Math.min(answer, result);
-
     }
 
-    // 선거구가 모두 연결되어있는지 확인.
-    static boolean isLinked(int num, List<Integer> list, int size) {
+    // 선거구가 모두 연결되어있는지 확인
+    static boolean isLinked(List<Integer> list) {
         boolean[] visited = new boolean[n+1];
-        visited[num] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(num);
+        Deque<Integer> q = new ArrayDeque<>();
+
+        visited[list.get(0)] = true;
+        q.add(list.get(0));
 
         int count = 1;
         while (!q.isEmpty()) {
             int start = q.poll();
 
             for (int i : graph[start]) {
-                // 이미 방문한 적이 없고, list에 속해야 함.
+                // 방문 이력 없고, 선거구에 해당
                 if (!visited[i] && list.contains(i)) {
                     visited[i] = true;
-                    count++;
                     q.add(i);
+                    count++;
                 }
             }
         }
 
-        if (count == size) {
-            return true;
-        }
-        return false;
+        return count == list.size() ? true : false;
     }
 }
