6a360e7cdb5a9023a04abfc13d64b65b998f393e
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-10 16:13:54 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -4,21 +4,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         char[] charArray = br.readLine().toCharArray();
+        Arrays.sort(charArray);
 
-        int[] numArray = new int[10];
-        for(int i = 0; i < charArray.length; i++) {
-            numArray[charArray[i] - '0']++;
-        }
-
-        for(int i = 9; i >= 0; i--) {
-            if(numArray[i] != 0) {
-                sb.append(String.valueOf(i).repeat(numArray[i]));
-            }
-        }
-
-        System.out.println(sb);
+        for(int i = charArray.length - 1; i >= 0; i--)
+            System.out.print(charArray[i]);
     }
 }
