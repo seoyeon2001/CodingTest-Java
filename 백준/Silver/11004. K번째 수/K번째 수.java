6d5119a52547b0ed56578adebd378da7ae81b163
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-10 18:32:24 +0900

    [Silver V] Title: K번째 수, Time: 1056 ms, Memory: 368376 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static int[] arr;
     static int k;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,34 +19,55 @@ public class Main {
         }
 
         // quick sorting
-        sort(0, n-1);
+        quickSortV2(0, n-1);
 
         System.out.println(arr[k-1]);
     }
 
-    static void sort(int left, int right) {
-        if(left >= right) return;
+    static void quickSortV2(int left, int right) {
+        if(left < right) {
+            int pivotIdx = partition(left, right);
 
-        int pivoit = arr[(left + right) / 2];
-        int l = left;
-        int r = right;
+            if(pivotIdx == k-1) return;
+            else if(k-1 < pivotIdx) {
+                quickSortV2(left, pivotIdx-1);
+            } else {
+                quickSortV2(pivotIdx+1, right);
+            }
+        }
+    }
 
-        while(l <= r) {
-            while(arr[l] < pivoit) l++;
-            while(arr[r] > pivoit) r--;
+    static int partition(int left, int right) {
+        // 데이터가 2개인 경우 바로 비교
+        if(left+1 == right) {
+            if(arr[left] > arr[right]) swap(left, right);
+            return right;
+        }
 
-            if(l <= r) {
-                int temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
+        int mid = (left + right) / 2;
+        swap(left, mid);
+        int pivot = arr[left];
+        int i = left+1, j = right;
+        while(i <= j) {
+            while(i <= right && arr[i] < pivot) i++;
+            while(j >= left + 1 && arr[j] > pivot) j--;
 
-                l++;
-                r--;
+            if(i <= j) {
+                swap(i, j);
+                i++;
+                j--;
             }
         }
 
-        if(k-1 <= r) sort(left, r);
-        else if(k-1 >= l) sort(l, right);
-        else return;
+        arr[left] = arr[j];
+        arr[j] = pivot;
+
+        return j;
+    }
+
+    static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
