5a8550234c3ee6d15707395c4d472527ba3b4b0a
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-10 13:48:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 15200 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -15,28 +15,29 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        select();
+        insertion();
 
         // 출력
         for(int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
     }
-    // 선택 정렬 O(n^2)
-    static void select() {
-        for(int start = 0; start < n-1; start++) {
-            int min = Integer.MAX_VALUE;
-            int minIdx = -1;
-            for(int i = start; i < n; i++) {
-                if(arr[i] < min) {
-                    min = arr[i];
-                    minIdx = i;
+
+    // 삽입 정렬 O(n^2)
+    static void insertion() {
+        for(int i = 1; i < n; i++) {
+            int num = arr[i];
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > num) {
+                    // shift
+                    for(int k = i; k > j; k--) {
+                        arr[k] = arr[k-1];
+                    }
+                    arr[j] = num;
+                    break;
                 }
             }
 
-            int temp = arr[start];
-            arr[start] = arr[minIdx];
-            arr[minIdx] = temp;
         }
     }
 }
