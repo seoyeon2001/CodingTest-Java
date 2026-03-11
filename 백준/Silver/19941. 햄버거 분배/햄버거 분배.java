e3d05e616d917ee775654939d52f3559f0ad6ae9
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-11 21:54:49 +0900

    [Silver III] Title: 햄버거 분배, Time: 112 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -2,53 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        String[] arr = br.readLine().split("");
-
-        Deque<Integer> p = new ArrayDeque<>();
-        Deque<Integer> h = new ArrayDeque<>();
+        char[] arr = br.readLine().toCharArray();
+        boolean[] eaten = new boolean[n];
 
         int answer = 0;
 
         for(int i = 0; i < n; i++) {
-            if(arr[i].equals("H")) {
-                if(!p.isEmpty()) {
-                    while(i - p.peek() > k) {
-                        p.poll();
-                    }
-                    if(!p.isEmpty()) {
-                        p.poll();
+            if(arr[i] == 'P') {
+
+                int left = Math.max(0, i - k);
+                int right = Math.min(n - 1, i + k);
+
+                for(int j = left; j <= right; j++) {
+                    if(arr[j] == 'H' && !eaten[j]) {
+                        eaten[j] = true;
                         answer++;
-                    } else {
-                        h.add(i);
+                        break;
                     }
-                } else {
-                    h.add(i);
-                }
-            } else {
-                if(!h.isEmpty()) {
-                    while(i - h.peek() > k) {
-                        h.poll();
-                    }
-                    if(!h.isEmpty()) {
-                        h.poll();
-                        answer++;
-                    } else {
-                        p.add(i);
-                    }
-                } else {
-                    p.add(i);
                 }
 
             }
         }
-        System.out.println(answer);
 
+        System.out.println(answer);
     }
 }
