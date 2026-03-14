098a938347f4e57175784d0423f4d195c124569c
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-14 20:14:55 +0900

    [Gold II] Title: 보석 도둑, Time: 1396 ms, Memory: 136668 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 보석 개수
+        int k = Integer.parseInt(st.nextToken()); // 가방 개수
+
+        List<int[]> jewelry = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken()); // 무게
+            int v = Integer.parseInt(st.nextToken()); // 가격
+
+            jewelry.add(new int[] {m, v});
+        }
+        Collections.sort(jewelry, (a, b) -> a[0] - b[0]); // 가벼운 순서대로
+
+        List<Integer> bags = new ArrayList<>();
+        for(int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken()); // 무게
+
+            bags.add(c);
+        }
+        Collections.sort(bags); // 가벼운 순서대로
+
+        long answer = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        int idx = 0;
+        for(int i = 0; i < k; i++) {
+            // 남은 보석이 없다면
+            if(jewelry.isEmpty()) break;
+
+            while(idx < n) {
+                // 보석이 가방보다 무거울 때
+                if(jewelry.get(idx)[0] > bags.get(i)) break;
+
+                pq.add(jewelry.get(idx));
+                idx++;
+            }
+
+            if(!pq.isEmpty()) {
+                answer += pq.poll()[1];
+            }
+        }
+        System.out.println(answer);
+    }
+}
