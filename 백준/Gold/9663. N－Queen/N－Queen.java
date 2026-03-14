33b9ece516f3c9da62be058776a3ee7f027b7486
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-15 03:03:05 +0900

    [Gold IV] Title: N-Queen, Time: 2664 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,45 +4,56 @@ import java.util.*;
 public class Main {
     static int answer;
     static int n;
-    static int[] arr;
+
+    static boolean[] col; // 같은 열
+    static boolean[] diag1; // ↙ 대각선 (r + c)
+    static boolean[] diag2; // ↘ 대각선 (r - c + n)
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        col = new boolean[n];
+        diag1 = new boolean[2 * n];
+        diag2 = new boolean[2 * n];
 
         answer = 0;
-        for(int c = 0; c < n; c++) { // 0행 c열
-            arr[0] = c;
-            calc(0); // 시작점
-        }
 
+        // 0행 시작
+        calc(0);
+        
         System.out.println(answer);
     }
 
     static void calc(int r) {
-        if(r == n-1) {
+        if(r == n) {
             answer++;
             return;
         }
 
         for(int i = 0; i < n; i++) {
-            if(!isAttack(r+1, i)) { // (r+1행, i열)에 두어도 공격당하지 않는다면
-                arr[r+1] = i;
+            if(!isAttack(r, i)) { // (r행, i열)에 두어도 공격당하지 않는다면
+                col[i] = true;
+                diag1[r + i] = true;
+                diag2[r - i + n] = true;
+
                 calc(r+1);
+
+                col[i] = false;
+                diag1[r + i] = false;
+                diag2[r - i + n] = false;
+
             }
         }
     }
 
     static boolean isAttack(int r, int c) {
-        for(int i = 0; i < r; i++) { // 행
-            // 열 체크
-            if(arr[i] == c) return true;
+        // 열 체크
+        if(col[c]) return true;
 
-            // 대각선 체크
-            if(r-i == Math.abs(c - arr[i])) return true;
-        }
+        // 대각선 체크
+        if(diag1[r + c] || diag2[r - c + n]) return true;
+        
         return false;
     }
 
