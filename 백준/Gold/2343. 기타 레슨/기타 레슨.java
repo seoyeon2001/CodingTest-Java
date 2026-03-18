3da5b1e51c5828250d80e3f4be7f021388dc2e30
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-18 18:57:20 +0900

    [Gold V] Title: 기타 레슨, Time: 268 ms, Memory: 23988 KB -BaekjoonHub

---
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int start = 0;
+        int end = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+
+            if(start < num) start = num;
+
+            end += num;
+        }
+
+//        System.out.println("start = " + start + " end = " + end);
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            int sum = 0;
+            int cnt = 0;
+
+            for(int i = 0; i < n; i++) {
+                if(sum + arr[i] > mid) {
+                    cnt++;
+                    sum = 0;
+                }
+                sum += arr[i];
+            }
+            if(sum != 0) cnt++;
+
+            if(cnt > m) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(start);
+    }
+}
