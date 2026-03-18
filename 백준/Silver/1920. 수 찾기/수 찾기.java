234243c9f66f5607c830e3cbb3da0047fb84eac4
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-18 17:33:33 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 56444 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,40 +4,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+           set.add(Integer.parseInt(st.nextToken()));
         }
-
-        Arrays.sort(arr); // 1 2 3 4 5
 
         int m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++) {
             int target = Integer.parseInt(st.nextToken());
-            boolean find = false;
 
-            int left = 0, right = n-1;
-
-            while(left <= right) {
-                int mid = (left + right) / 2;
-
-                if(arr[mid] < target) {
-                    left = mid+1;
-                } else if(arr[mid] > target) {
-                    right = mid-1;
-                } else {
-                    find = true;
-                    break;
-                }
+            if(set.contains(target)) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
             }
-
-            System.out.println(find ? 1 : 0);
         }
+        System.out.println(sb);
     }
 }
