07e68ae4ce3360a8a7378dbf4003e10d7a818dd2
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-22 18:53:54 +0900

    [Gold V] Title: 최소비용 구하기, Time: 468 ms, Memory: 50420 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        List<int[]>[] list = new ArrayList[n+1];
+        for(int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            list[s].add(new int[] {e, cost});
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        int[] answer = new int[n+1];
+        Arrays.fill(answer, Integer.MAX_VALUE);
+
+        boolean[] visited = new boolean[n+1];
+        
+        answer[start] = 0;
+        q.add(new int[] {start, 0});
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            
+            if(visited[cur[0]]) continue;
+            
+            visited[cur[0]] = true;
+            for(int[] next : list[cur[0]]) {
+                if(answer[next[0]] > answer[cur[0]] + next[1]) {
+                    answer[next[0]] = answer[cur[0]] + next[1];
+                    q.add(new int[] {next[0], answer[next[0]]});
+                }
+            }
+        }
+        System.out.println(answer[end]);
+    }
+}
