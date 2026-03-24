6e891dee024445812236fdf838bfdb0a5732c698
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-24 13:54:35 +0900

    [Silver III] Title: 소수 구하기, Time: 1008 ms, Memory: 29188 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -5,20 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        
-        for(int i = M; i <= N; i++) {
-            if (isPrime(i)) System.out.println(i);
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        for(int i = m; i <= n; i++) {
+            if(isPrime(i)) {
+                System.out.println(i);
+            }
         }
     }
-    
+
     static boolean isPrime(int num) {
-        if(num < 2) return false;
-        
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
+        if(num == 1) return false;
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
         }
         return true;
     }
