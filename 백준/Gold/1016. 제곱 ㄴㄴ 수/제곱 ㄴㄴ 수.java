c0be50fd572f71c3e05a3be4f9f17a30b5854277
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-26 14:36:34 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 172 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -2,23 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
 
-        Set<Long> set = new HashSet<>();
+        boolean[] nums = new boolean[(int) (max - min + 1)];
+
         for(long i = 2; i <= Math.sqrt(max); i++) {
             long num = i * i;
-            for(long j = min / num; j <= max / num; j++) {
-                if(num * j < min) continue;
-                set.add(num*j);
+
+            for(long j = min / num; j <= max / num ; j++) {
+                if(num*j-min < 0) continue;
+                nums[(int)(num*j-min)] = true;
             }
         }
-//        System.out.println("set = " + set);
-//        System.out.println("size = " + set.size());
-        System.out.println(max-min+1-set.size());
+
+        long answer = 0;
+        for(int i = 0; i < max - min + 1; i++) {
+            if(!nums[i]) answer++;
+        }
+
+        System.out.println(answer);
     }
 }
