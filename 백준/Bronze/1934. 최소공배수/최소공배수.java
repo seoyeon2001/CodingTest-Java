2963333fa33863af31136a0c4e69d63eac83cdd2
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-03-26 16:25:48 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 최대공약수 GCD 구하기
+            int gcd = 1;
+            if(a > b) gcd = gcd(a, b);
+            else if(a < b) gcd = gcd(b, a);
+            else gcd = a;
+
+            sb.append(a*b / gcd).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static int gcd(int n1, int n2) {
+        int rest = n1 % n2;
+
+        if(rest != 0) return gcd(n2, rest);
+
+        return n2;
+    }
+}
