d78e956d951a3918da6755b8686b4f34b54287b7
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-02 00:01:44 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -10,19 +10,13 @@ public class Main {
         int[] cnt = new int[n+1];
 
         for(int i = 2; i <= n; i++) {
-            int answer = i-1;
+            cnt[i] = cnt[i-1]+1;
 
-            if(i % 3 == 0 && i % 2 == 0) {
-                answer = Math.min(cnt[i-1], Math.min(cnt[i/3], cnt[i/2]));
-            } else if(i % 3 == 0 && i % 2 != 0) {
-                answer = Math.min(cnt[i-1], cnt[i/3]);
-            } else if(i % 3 != 0 && i % 2 == 0) {
-                answer = Math.min(cnt[i-1], cnt[i/2]);
-            } else {
-                answer = cnt[i-1];
+            if(i % 3 == 0) {
+                cnt[i] = Math.min(cnt[i], cnt[i/3]+1);
+            } if(i % 2 == 0) {
+                cnt[i] = Math.min(cnt[i], cnt[i/2]+1);
             }
-
-            cnt[i] = answer+1;
 //            System.out.println("cnt = " + Arrays.toString(cnt));
         }
 
