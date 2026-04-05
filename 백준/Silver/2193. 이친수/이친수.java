e9e929255243a9b1b6567576d84417d13b702feb
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-05 17:37:13 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -4,16 +4,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine());
-        
-        long[] dp = new long[n+1];
-        dp[1] = 1;
-        
+
+        long[][] dp = new long[n+1][2];
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-2] + dp[i-1];
+            dp[i][0] = dp[i-1][1] + dp[i-1][0];
+            dp[i][1] = dp[i-1][0];
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[n][0] + dp[n][1]);
     }
 }
