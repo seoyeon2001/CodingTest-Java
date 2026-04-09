1ebfb90c3990d87d535c1ac04c9a43969a2635d4
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-09 15:23:25 +0900

    [Silver II] Title: 한 줄로 서기, Time: 120 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -8,35 +8,26 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        boolean[] visited = new boolean[n];
         int[] ans = new int[n];
 
-        int start = 1;
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             int idx = Integer.parseInt(st.nextToken());
 
             int time = 0;
             for(int j = 0; j < n; j++) {
-                if(!visited[j]) {
+                if(ans[j] == 0) {
                     if(idx == time) {
-                        ans[j] = start;
-                        visited[j] = true;
+                        ans[j] = i;
                         break;
                     }
-
                     time++;
                 }
             }
-
-            start++;
         }
 
         for(int i : ans) {
             System.out.print(i + " ");
         }
-
-
-
     }
 }
