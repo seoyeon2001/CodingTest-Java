b51138175f172bb919c139550c339d8f6d34ac15
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-12 12:51:34 +0900

    [Gold V] Title: 집합의 표현, Time: 916 ms, Memory: 53484 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,53 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
-    
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        parent = new int[n+1];
+        arr = new int[n+1];
         for(int i = 0; i <=n; i++) {
-            parent[i] = i;
+            arr[i] = i;
         }
-        
+
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int check = Integer.parseInt(st.nextToken());
-            int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
+            int type = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if(check == 1) {
-                // 출력해야 함
-                sb.append(find(parent[num1]) == find(parent[num2]) ? "YES" : "NO").append("\n");
-            } else if(check == 0) {
-                // 합해야 함
-                union(num1, num2);
+            if(type == 0) {
+                union(a, b);
+            } else {
+                System.out.println(find(a) == find(b) ? "YES" : "NO");
+
             }
         }
-        System.out.println(sb);
-    }
-    static int find(int num) {
-        if(num != parent[num]){
-            parent[num] = find(parent[num]);
-        }
-        return parent[num];
     }
 
-    static void union(int num1, int num2) {
-        num1 = find(num1);
-        num2 = find(num2);
-        
-        if(num1 < num2) {
-            parent[num2] = num1;
-        } else if(num2 < num1) {
-            parent[num1] = num2;
-        }
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a < b) arr[b] = a;
+        else arr[a] = b;
+    }
+
+    static int find(int num) {
+        if(arr[num] == num) return arr[num];
+        return arr[num] = find(arr[num]);
     }
 }
