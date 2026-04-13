3c4553edaec65c6029e8ad7c45c005ff3d4a8cc6
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-14 01:30:35 +0900

    [level 2] Title: 배달, Time: 7.07 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        
+        int[][] map = new int[N+1][N+1];
+        for(int i = 0; i <= N; i++) {
+            Arrays.fill(map[i], Integer.MAX_VALUE);
+        }
+        
+        for(int[] r : road) {
+            map[r[0]][r[1]] = Math.min(map[r[0]][r[1]], r[2]);
+            map[r[1]][r[0]] = Math.min(map[r[0]][r[1]], r[2]);
+        }
+        
+        for(int i = 0; i <= N; i++) {
+            map[i][i] = 0;
+        }
+        
+        // System.out.println(Arrays.deepToString(map));
+        
+        for(int k = 1; k <= N; k++) {
+            for(int i = 1; i <= N; i++) {                
+                for(int j = 1; j <= N; j++) {
+                    // System.out.println(i + " " + j + " " + k);
+                    if(map[i][k] + map[k][j] >= 0 && map[i][k] + map[k][j] < map[i][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                    
+                }
+                // System.out.println(Arrays.deepToString(map));
+            }
+        }
+        
+        // System.out.println(Arrays.deepToString(map));
+        
+        for(int i = 1; i <= N; i++) {
+            if(map[1][i] <= K) answer++;
+        }
+
+        return answer;
+    }
+}
