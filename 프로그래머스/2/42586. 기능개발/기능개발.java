4f973cf01d6b4ee0fc5157e6e4848fc22e65815e
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-17 12:53:13 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int[] days = new int[progresses.length];
+        for(int i = 0 ; i < progresses.length; i++) {
+            int left = 100 - progresses[i];
+            
+            days[i] = left / speeds[i];
+            
+            if(left % speeds[i] != 0) days[i]++;
+            
+        }
+        // System.out.println(Arrays.toString(days));
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(days[0]);
+        
+        for(int i = 1; i < days.length; i++) {
+            if(days[i] <= q.peek()) {
+                q.add(days[i]);
+            } else {
+                list.add(q.size());
+                q.clear();
+                q.add(days[i]);
+            } 
+        }
+        list.add(q.size());
+        
+        // System.out.println(list);
+        
+        answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
