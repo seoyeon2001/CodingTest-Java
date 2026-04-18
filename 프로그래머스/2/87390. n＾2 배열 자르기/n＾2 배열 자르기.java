b6f11d68f026995c70136c85b5cfccb04802d7c8
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-18 22:23:19 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.71 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -4,25 +4,6 @@ class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int) (right - left + 1)];
         
-        // int[] arr = new int[n*n];
-        // for(int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         arr[n*i+j] = Math.max(i, j) + 1;
-        //     }
-        // }
-        
-        // System.out.println(Arrays.toString(arr));
-        
-        // for(int i = (int) left; i <= (int) right; i++) {
-        //     answer[(int) (i-left)] = arr[i];
-        // }
-        
-        long[] start = {left / n, left % n};
-        long[] end = {right / n, right % n};
-        
-        // System.out.println(Arrays.toString(start));
-        // System.out.println(Arrays.toString(end));
-        
         int tmp = 0;
         for(long idx = left; idx <= right; idx++) {
             int num = (int) Math.max(idx / n, idx % n) + 1;
@@ -32,3 +13,16 @@ class Solution {
         return answer;
     }
 }
+
+// int[] arr = new int[n*n];
+// for(int i = 0; i < n; i++) {
+//     for (int j = 0; j < n; j++) {
+//         arr[n*i+j] = Math.max(i, j) + 1;
+//     }
+// }
+
+// System.out.println(Arrays.toString(arr));
+
+// for(int i = (int) left; i <= (int) right; i++) {
+//     answer[(int) (i-left)] = arr[i];
+// }
