d59ef5c50c0381c14c130a36196adbba73f3106e
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-27 19:55:32 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,42 @@
+// 14분
+// class Solution {
+//     public int[] solution(int brown, int yellow) {
+//         int[] answer = new int[2];
+        
+//         int row = 1;
+//         int col = 1;
+        
+//         for(int i = 1; i < (brown - 4) / 2; i++) {
+//             row = i;
+//             col = (brown - 4) / 2 - i;
+//             // System.out.println(row + " " + col);
+            
+//             if(row * col == yellow) {
+//                 answer[0] = Math.max(row + 2, col + 2);
+//                 answer[1] = Math.min(row + 2, col + 2);
+//                 break;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        int row = 1;
-        int col = 1;
-        
-        for(int i = 1; i < (brown - 4) / 2; i++) {
-            row = i;
-            col = (brown - 4) / 2 - i;
-            // System.out.println(row + " " + col);
-            
-            if(row * col == yellow) {
-                answer[0] = Math.max(row + 2, col + 2);
-                answer[1] = Math.min(row + 2, col + 2);
+        for (int height = 1; height * height <= yellow; height++) {
+            if (yellow % height != 0) continue;
+
+            int width = yellow / height;
+
+            // 테두리 갈색 개수 확인
+            if ((width + height) * 2 + 4 == brown) {
+                answer[0] = width + 2;
+                answer[1] = height + 2;
                 break;
             }
         }
+
         return answer;
     }
 }
