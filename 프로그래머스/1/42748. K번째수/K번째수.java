a07ea787cd7f71fb734b4b825c5ee5c1f75390a9
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-04-28 00:45:58 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int start = command[0];
+            int end = command[1];
+            int target = command[2];
+            
+            int[] arr = new int[end - start + 1];
+            for(int j = start-1; j < end; j++) {
+                arr[j-start+1] = array[j];
+            }
+            // System.out.println(Arrays.toString(arr));
+            Arrays.sort(arr);
+            answer[i] = arr[target-1];
+        }
+        return answer;
+    }
+}
