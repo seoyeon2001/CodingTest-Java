640d83d153858af387bb09a840b5e90e5d108a68
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-05-13 00:23:46 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.80 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 56.4 MB, 시간: 14.47 ms
+메모리: 57.2 MB, 시간: 4.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 23:53:55
+2026년 05월 13일 00:23:42
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,56 @@
 import java.util.*;
 
 class Solution {
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int[] dr = {0, 0, 1, -1};
+    static int[] dc = {1, -1, 0, 0};
+    static boolean[][] visited;
+    static int[][] maps;
+    static Deque<int[]> q = new ArrayDeque<>();
+    static int answer = -1;
+    static int n;
+    static int m;
     
-    public int solution(int[][] maps) {
+    public int solution(int[][] maps) {        
+        this.maps = maps;
+        this.n = maps.length;
+        this.m = maps[0].length;
+        visited = new boolean[n][m];
         
-        int n = maps.length;
-        int m = maps[0].length;
-       
-        Deque<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
-        
-        q.add(new int[] {0, 0, 1}); // 행, 열, 칸 개수
+        q.add(new int[] {0, 0, 1});
         visited[0][0] = true;
         
+        bfs();
+        
+        return answer;
+    }
+    
+    static private void bfs() {
+        
         while(!q.isEmpty()) {
+            
             int[] cur = q.poll();
             int r = cur[0];
             int c = cur[1];
             int cnt = cur[2];
             
-            if (r == n-1 && c == m-1) return cnt;
+            if(r == n-1 && c == m-1) {
+                answer = cnt;
+                return;
+            }
             
             for(int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] != 0 && !visited[nr][nc]) {
-                    q.add(new int[] {nr, nc, cnt + 1});
-                    visited[nr][nc] = true;                    
+
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+
+                if(!visited[nr][nc] && maps[nr][nc] != 0) {
+                    visited[nr][nc] = true;
+                    q.add(new int[] {nr, nc, cnt+1});
                 }
             }
         }
-        return -1;
+        
+        
     }
 }
