20eb13b5c9cdc23c8cabb5c530ad22f7e9e1fdce
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-06-17 15:49:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 7.82 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        // System.out.println("Hello Java");
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int size = A.length;        
+        for(int i = 0; i < size; i++) {
+            answer+= A[i] * B[size-i-1];
+        }
+
+        return answer;
+    }
+}
