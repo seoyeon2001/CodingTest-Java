13498503e1c7c02dfa3640eb5195a7df4a12f36d
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-06-17 15:42:24 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.15 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,44 +1,26 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String s) {
-//         StringBuffer sb = new StringBuffer();
-//         boolean isNextSpace = true;
-        
-//         for(int i = 0; i < s.length(); i++) {
-//             if(s.charAt(i) == ' ') {
-//                 sb.append(" ");
-//                 isNextSpace = true;
-//             }
-            
-//             else if(isNextSpace) {
-//                 if(Character.isLetter(s.charAt(i))) {
-//                     sb.append(Character.toUpperCase(s.charAt(i)));
-//                 } else {
-//                     sb.append(s.charAt(i));
-//                 }
-//                 isNextSpace = false;
-//             } 
-            
-//             else {
-//                 sb.append(Character.toLowerCase(s.charAt(i)));
-//             }
-//         }
-        
-//         return sb.toString();
-        
         String answer = "";
-        String[] sp = s.toLowerCase().split("");
-        // System.out.println(Arrays.toString(sp));
-        
-        boolean flag = true;
-
-        for(String ss : sp) {
-            answer += flag ? ss.toUpperCase() : ss;
-            flag = ss.equals(" ") ? true : false;
+        String[] arr = s.split(" ");
+ 
+        for(int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+ 
+            if(word.length() == 0) {
+                answer += " ";
+            } else {
+                answer += word.substring(0, 1).toUpperCase();
+                answer += word.substring(1, word.length()).toLowerCase();
+                answer += " ";
+            }
         }
-
-        return answer;
+        
+        if(s.substring(s.length() - 1, s.length()).equals(" ")){
+            return answer;
+        }
+        
+        return answer.substring(0, answer.length() - 1);
     }
 }
