152705f4190501f44870af711329ea576647047d
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-06-17 16:10:41 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 31.51 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        
+        int removeZeroCnt = 0;
+        int time = 0;
+        while(true) {
+            if(s.length() == 1) break;
+            
+            int length = s.length();
+            int nextLength = 0;
+            String[] arr = s.split("");
+            for(int i = 0; i < arr.length; i++) {
+                int num = Integer.parseInt(arr[i]);
+                if(num == 1) nextLength += 1;
+                else removeZeroCnt += 1;
+            }
+            
+            s = Integer.toBinaryString(nextLength);
+            time++;
+        }
+        
+        answer[0] = time;
+        answer[1] = removeZeroCnt;
+        
+        return answer;
+    }
+}
