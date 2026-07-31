403401d344d95ed988de344e82b6e4e6a4f7e388
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-07-31 21:25:34 +0900

    [level 2] Title: 피로도, Time: 7.41 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,107 +2,47 @@ import java.util.*;
 
 class Solution {
     static int answer = 0;
-    static int length;
+    static int n;
     
     public int solution(int k, int[][] dungeons) {
-        length = dungeons.length;
+        n = dungeons.length;
         
-        permutation(0, new ArrayList<>(), new boolean[length], dungeons, k);
-        return answer;
-    }
-    
-    private void permutation(int depth, List<Integer> list, boolean[] visited, int[][] dungeons, int k) {
-        if(depth == length) {
-            int cnt = calc(list, dungeons, k);
-            // System.out.println(cnt);
-            answer = Math.max(answer, cnt);
-        }
-        
-        for(int i = 0; i < length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                list.add(i);
-                
-                permutation(depth+1, list, visited, dungeons, k);
-                
-                visited[i] = false;
-                list.remove(list.size()-1);
-            }
-        }
-    }
-    
-    private int calc(List<Integer> list, int[][] dungeons, int k) {
-        int cnt = 0;
-        
-        for(int idx : list) {
-            if(k >= dungeons[idx][0]) {
-                k -= dungeons[idx][1];
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-}
-
-
-
-
-
-
-/*
-// 24분
-import java.util.*;
-
-class Solution {
-    static int answer = 0;
-    
-    public int solution(int k, int[][] dungeons) {
-        
-        permutation(k, dungeons, dungeons.length, new boolean[dungeons.length], new ArrayList<>());
+        permutation(k, dungeons, new ArrayList<>(), new boolean[n]);
         
         return answer;
     }
     
-    static void permutation(int k, int[][] dungeons, int n, boolean[] visited, List<Integer> list) {
+    static void permutation(int k, int[][] dungeons, List<Integer> list, boolean[] visited) {
         if(list.size() == n) {
             // System.out.println(list);
-            int res = calc(k, dungeons, list);
             
-            if(res > answer) {
-                answer = res;
-            }
+            calc(k, dungeons, list);
+            return;
         }
         
         for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                list.add(i);
-                
-                permutation(k, dungeons, n, visited, list);
-                
-                visited[i] = false;
-                list.remove(list.size() - 1);
-            }
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            list.add(i);
+            
+            permutation(k, dungeons, list, visited);
+            
+            int num = list.remove(list.size()-1);
+            visited[num] = false;
         }
     }
     
-    static int calc(int k, int[][] dungeons, List<Integer> list) {
+    static void calc(int k, int[][] dungeons, List<Integer> idxs) {
+        int cnt = 0;
         
-        int answer = 0;
-        
-        for(int idx : list) {
-            int min = dungeons[idx][0];
-            int minus = dungeons[idx][1];
-            
-            if(k >= min) {
-                k -= minus;
-                answer++;
-            }
+        for(int idx : idxs) {
+            if(k >= dungeons[idx][0]) {
+                k -= dungeons[idx][1];
+                cnt++;
+            } else break;
         }
         
-        // System.out.println(answer);
-        
-        return answer;        
+        if(answer < cnt) answer = cnt;
     }
 }
-*/
