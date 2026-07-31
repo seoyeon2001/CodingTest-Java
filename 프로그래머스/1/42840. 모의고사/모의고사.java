e7f1ce76592cc6ecfc6117ce1041ec0c587946fa
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-07-31 18:35:02 +0900

    [level 1] Title: 모의고사, Time: 0.49 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+        
         int[] one = {1, 2, 3, 4, 5}; // 5
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
         
-        int A = 0;
-        int B = 0;
-        int C = 0;
+        int on = 0;
+        int tw = 0;
+        int th = 0;
+        
         for(int i = 0; i < answers.length; i++) {
-            int answer = answers[i];
-            
-            if(answer == one[i%5]) A++;
-            if(answer == two[i%8]) B++;
-            if(answer == three[i%10]) C++;
+            if(answers[i] == one[i%5]) on++;
+            if(answers[i] == two[i%8]) tw++;
+            if(answers[i] == three[i%10]) th++;
         }
-        // System.out.println(A + " " + B + " " + C);
-        int max = Math.max(A, Math.max(B, C));
+        
+        int max = Math.max(on, Math.max(tw, th));
         
         List<Integer> list = new ArrayList<>();
-        if(A == max) list.add(1);
-        if(B == max) list.add(2);
-        if(C == max) list.add(3);
-        
+        if(on == max) list.add(1);
+        if(tw == max) list.add(2);
+        if(th == max) list.add(3);
         // System.out.println(list);
         
-        int[] result = new int[list.size()];
+        int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+            answer[i] = list.get(i);
         }
-        
-        return result;
+        return answer;
     }
 }
