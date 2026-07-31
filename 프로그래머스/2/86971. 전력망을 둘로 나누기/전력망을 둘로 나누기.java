2d16bbdc2de1300b0e7ef2bcef67b980d74b269d
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-07-31 22:12:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.54 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,61 +1,57 @@
 import java.util.*;
 
 class Solution {
+    static int answer;
+    static List<Integer>[] list;
+    
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        // 기본 세팅
-        List<Integer>[] graph = new ArrayList[n+1];
-        for(int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for(int[] wire : wires) {
-            graph[wire[0]].add(wire[1]);
-            graph[wire[1]].add(wire[0]);
-        }
-        // System.out.println(Arrays.toString(graph));
+        answer = Integer.MAX_VALUE;
         
-        // 시작
+        list = new ArrayList[n+1];
+        for(int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        
         for(int[] wire : wires) {
-            graph[wire[0]].remove(Integer.valueOf(wire[1]));
-            graph[wire[1]].remove(Integer.valueOf(wire[0]));
+            list[wire[0]].add(wire[1]);
+            list[wire[1]].add(wire[0]);
+        }
+        
+        for(int[] wire : wires) {
+            list[wire[0]].remove(Integer.valueOf(wire[1]));
+            list[wire[1]].remove(Integer.valueOf(wire[0]));
             
-            // System.out.println(Arrays.toString(graph));
+            calc(list, n);
             
-            int cnt1 = count(graph, n);
-            int cnt2 = n - cnt1;
-            int cntR = cnt1 - cnt2;
-            answer = Math.min(answer, Math.abs(cntR));
-            
-            graph[wire[0]].add(wire[1]);
-            graph[wire[1]].add(wire[0]);
+            list[wire[0]].add(wire[1]);
+            list[wire[1]].add(wire[0]);
         }
         
         return answer;
     }
     
-    static int count(List<Integer>[] graph, int n) {
-        int cnt = 0;
-        
-        Deque<Integer> q = new ArrayDeque<>();
+    static void calc(List<Integer>[] list, int n) {        
         boolean[] visited = new boolean[n+1];
-        
-        q.add(1);
         visited[1] = true;
         
-        while(!q.isEmpty()) {
-            int num = q.poll();
-            
-            for(int node : graph[num]) {
-                if(!visited[node]) {
-                    q.add(node);
-                    visited[node] = true;
-                }
+        int cnt = dfs(1, visited, list);
+        
+        if(answer > Math.abs((n-cnt)-cnt)) answer = Math.abs((n-cnt)-cnt);
+    }
+    
+    static int dfs(int num, boolean[] visited, List<Integer>[] list) {
+        int cnt = 1;
+        
+        visited[num] = true;
+        
+        for(int link : list[num]) {
+            if(!visited[link]) {
+                visited[link] = true;
+                
+                cnt += dfs(link, visited, list);
             }
         }
         
-        for(boolean b : visited) {
-            if (b) cnt++;
-        }
         return cnt;
     }
 }
