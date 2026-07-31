cac67bb98431ead9db501f5c935ce535e7f2f500
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2026-07-31 21:12:21 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,70 +1,20 @@
-// 14분
-// class Solution {
-//     public int[] solution(int brown, int yellow) {
-//         int[] answer = new int[2];
-        
-//         int row = 1;
-//         int col = 1;
-        
-//         for(int i = 1; i < (brown - 4) / 2; i++) {
-//             row = i;
-//             col = (brown - 4) / 2 - i;
-//             // System.out.println(row + " " + col);
-            
-//             if(row * col == yellow) {
-//                 answer[0] = Math.max(row + 2, col + 2);
-//                 answer[1] = Math.min(row + 2, col + 2);
-//                 break;
-//             }
-//         }
-//         return answer;
-//     }
-// }
+import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        for (int height = 1; height * height <= yellow; height++) {
-            if (yellow % height != 0) continue;
-
-            int width = yellow / height;
-
-            // 테두리 갈색 개수 확인
-            if ((width + height) * 2 + 4 == brown) {
-                answer[0] = width + 2;
-                answer[1] = height + 2;
-                break;
-            }
-        }
-
-        return answer;
-    }
-}
-
-/*
-class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
         
-        // 2a + 2b = brown + 4
-        // a + b = brown/2 + 2
+        int x = 0;
+        int y = 0;
         
-        for (int a = 1; a < brown/2 + 2; a++) {
-            int b = brown/2 + 2 - a;
+        int calc = (brown - 4) / 2;
+        
+        for(int i = 1; i < calc; i++) {
+            y = i;
+            x = calc - y;
             
-            if ((a-2) * (b-2) == yellow) {
-                if (a > b) {
-                    answer[0] = a;
-                    answer[1] = b;
-                } else {
-                    answer[0] = b;
-                    answer[1] = a;
-                }
-                
-                break;
-            }
+            if(x * y == yellow) break;
         }
-        return answer;
+        
+        return new int[] {x+2, y+2};
     }
 }
-*/
